bae7b93febeb0bb78e93216ea0a7fbe33bc39f24
caio reis <gameplayers176@gmail.com>
2024-04-12 14:35:04 -0300

    teste

---
+++ YellowCloaker-master/admin/version.php
@@ -0,0 +1,8 @@
+<h2>
+    <a href="https://yellowweb.top/donate" target="_blank">
+        Version: 09.12.2023
+        <br />
+        PHP: <?= phpversion() ?>
+        <br /> Please, donate!
+    </a>
+</h2>
